ae977a247da0b913d82cb54c155c3082ef10be40
Eber <www.eberacunaquispe@gmail.com>
2025-10-18 20:07:36 -0500

    funcion que simula las rondas del juego

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ function getHumanChoice(){
     let ingreso = prompt("Ingrese piedra, papel o tijera").toLowerCase();
     return ingreso
 }
+
+function playRound(humanChoice, computerChoice){
+    if(humanSelection && computerSelection == "tijera"){
+        console.log("Empate")
+    }
+    if(humanSelection == "tijera" && computerSelection == "papel"){
+        console.log("Tu ganas, Tijera vence al papel")
+        humanScore+=1;
+    } 
+    if(humanSelection == "tijera" && computerSelection == "piedra"){
+        console.log("Pierdes, Piedra vence a tijera")
+        computerScore+=1;
+    } 
+
+    if(humanSelection == "piedra" && computerSelection == "piedra"){
+        console.log("Empate")
+    } 
+
+    if(humanSelection == "piedra" && computerSelection == "tijera"){
+        console.log("Tu ganas, Piedra vence a tijera")
+        humanScore+=1;
+    } 
+    
+    if(humanSelection == "piedra" && computerSelection == "papel"){
+        console.log("Pierdes, Papel vence a piedra")
+        computerScore+=1;
+    }
+
+    if(humanSelection == "papel" && computerSelection == "papel"){
+        console.log("Empate")
+    }
+
+    if(humanSelection == "papel" && computerSelection == "piedra"){
+        console.log("Tu ganas, Papel vence a piedra")
+        humanScore+=1;
+    }
+
+    if(humanSelection == "papel" && computerSelection == "tijera"){
+        console.log("Pierdes, Tijera vence a piedra")
+        computerScore+=1;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
