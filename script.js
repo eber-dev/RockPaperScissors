5428e44a968c1fd8ff00579a20daea21a5971ed4
Eber <www.eberacunaquispe@gmail.com>
2025-10-19 19:14:54 -0500

    Añadiendo los mensajen en consola de la funcion playRound

--- script.js
+++ script.js
@@ -4,20 +4,16 @@ function getComputerChoice(){
     let opciones = Math.floor(Math.random()*3)+1
     switch(opciones){
         case 1:
-            console.log("Computer selection: piedra")
             return "piedra"
         case 2:
-            console.log("Computer selection: papel")
             return "papel"
         case 3:
-            console.log("Computer selection: tijera")
             return "tijera"
     }
 }
 
 function getHumanChoice(){
     let ingreso = prompt("Ingrese piedra, papel o tijera").toLowerCase();
-    console.log("Tu eleccion: ",ingreso)
     return ingreso
 }
 const humanSelection = getHumanChoice();
@@ -27,34 +23,58 @@ function playRound(humanChoice, computerChoice){
     switch(humanSelection){
         case "piedra":
             if(computerSelection === "piedra"){
+                console.log("Tu seleccion: piedra")
+                console.log("Computer selection: piedra")
                 console.log("empate")
             }
             if(computerSelection === "papel"){
+                console.log("Tu seleccion: piedra")
+                console.log("Computer selection: papel")
+                computerScore+=1
                 console.log("pierdes")
             }
             if(computerSelection === "tijera"){
+                console.log("Tu seleccion: piedra")
+                console.log("Computer selecion: tijera")
+                humanScore+=1
                 console.log("ganas")
             }
             break;
         case "papel":
             if(computerSelection === "piedra"){
+                console.log("Tu seleccion: papel")
+                console.log("Computer selection: piedra")
+                humanScore+=1
                 console.log("ganas")
             }
             if(computerSelection === "papel"){
+                console.log("Tu seleccion: papel")
+                console.log("Computer selection: papel")
                 console.log("empate")
             }
             if(computerSelection === "tijera"){
+                console.log("Tu seleccion: papel")
+                console.log("Computer selection: tijera")
+                computerScore+=1
                 console.log("pierdes")
             }
             break;
         case "tijera":
             if(computerSelection === "piedra"){
+                console.log("Tu seleccion: tijera")
+                console.log("Computer selection: piedra")
+                computerScore+=1
                 console.log("pierdes")
             }
             if(computerSelection === "papel"){
+                console.log("Tu seleccion: tijera")
+                console.log("Computer selection: papel")
+                humanScore+=1
                 console.log("ganas")
             }
             if(computerSelection === "tijera"){
+                console.log("Tu seleccion: tijera")
+                console.log("Computer selection: tijera")
                 console.log("empate")
             }
             break;
@@ -63,10 +83,4 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-
-
-playRound(humanSelection, computerSelection);
-
-function playGame(){
-
-}
+playRound(humanSelection,computerSelection)
