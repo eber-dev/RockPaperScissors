f19aaaa49956a9b1d444fd78aac98dbf38ff1d0f
Eber <www.eberacunaquispe@gmail.com>
2026-01-14 17:14:21 -0500

    Iterando los botones para capturar su valor

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function getComputerChoice(){
 
 const container = document.querySelector(".container")
 const botones = document.querySelectorAll(".btn")
+let contador
 
 const campo = document.createElement("div")
 campo.classList.add("campo")
@@ -21,12 +22,54 @@ campo.style.flexDirection = "row"
 campo.style.justifyContent = "center"
 campo.style.whiteSpace = "pre-line"
 
+function comparar(a,b){
+    switch(a){
+        case "Piedra":
+            if(b == "Piedra"){
+                return "empate";
+            }
+            if(b == "Papel"){
+                return "Pierdes"
+            }
+            if(b == "Tijera"){
+                return "Ganas"
+            }
+            break
+        case "Papel":
+            if(b == "Papel"){
+                return "empate"
+            }
+            if(b == "Piedra"){
+                return "Ganas"
+            }
+            if(b == "Tijera"){
+                return "Pierdes"
+            }
+            break
+        case "Tijera":
+            if(b == "Tijera"){
+                return "empate"
+            }
+            if(b == "Papel"){
+                return "Ganas"
+            }
+            if(b == "Piedra"){
+                return "Pierdes"
+            }
+            break
+        default:
+            console.log("No se reconocio la eleccion")
+    }
+}
+
 botones.forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
         let valor = e.target.value
         let computerSelection = getComputerChoice()
     })
 })
+
+
    
 
 container.appendChild(campo)
