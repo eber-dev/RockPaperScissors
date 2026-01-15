8a20e3d83591c5feb3e35ec8f11e102e7251b63e
Eber <www.eberacunaquispe@gmail.com>
2026-01-15 08:59:10 -0500

    Logica funcional en consola del juego

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ function getComputerChoice(){
 
 const container = document.querySelector(".container")
 const botones = document.querySelectorAll(".btn")
-let contador
+let contador = 1
+let puntaje_humano = 0
+let puntaje_pc = 0
+let puntaje_jugadores = new Array(2)
 
 const campo = document.createElement("div")
 campo.classList.add("campo")
@@ -26,34 +29,43 @@ function comparar(a,b){
     switch(a){
         case "Piedra":
             if(b == "Piedra"){
+                console.log("empate")
                 return "empate";
             }
             if(b == "Papel"){
+                console.log("Pierdes")
                 return "Pierdes"
             }
             if(b == "Tijera"){
+                console.log("Ganas")
                 return "Ganas"
             }
             break
         case "Papel":
             if(b == "Papel"){
+                console.log("empate")
                 return "empate"
             }
             if(b == "Piedra"){
+                console.log("Ganas")
                 return "Ganas"
             }
             if(b == "Tijera"){
+                console.log("Pierdes")
                 return "Pierdes"
             }
             break
         case "Tijera":
             if(b == "Tijera"){
+                console.log("empate")
                 return "empate"
             }
             if(b == "Papel"){
+                console.log("Ganas")
                 return "Ganas"
             }
             if(b == "Piedra"){
+                console.log("Pierdes")
                 return "Pierdes"
             }
             break
@@ -62,10 +74,51 @@ function comparar(a,b){
     }
 }
 
+function puntaje(a){
+    switch(a){
+        case "Ganas":
+            puntaje_humano++
+            console.log(`El Score humano es: ${puntaje_humano}`)
+            break
+        case "Pierdes":
+            puntaje_pc++
+            console.log(`El Score pc es: ${puntaje_pc}`)
+            break
+        case "empate":
+            console.log(`El Score humano es: ${puntaje_humano}`)
+            console.log(`El Score pc es: ${puntaje_pc}`)
+            break
+    }
+    return [puntaje_humano,puntaje_pc]   
+}
+
+function final(a){
+    if(a[0]>a[1]){
+        console.log(`PC: ${a[1]}`)
+        console.log(`TU: ${a[0]}`)
+        console.log("TU GANAS")
+    }
+    if(a[0]<a[1]){
+        console.log(`PC: ${a[1]}`)
+        console.log(`TU: ${a[0]}`)
+        console.log("TU PIERDES")
+    }
+}
+
 botones.forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
         let valor = e.target.value
         let computerSelection = getComputerChoice()
+        let comparacion = comparar(valor,computerSelection)
+        puntaje_jugadores = puntaje(comparacion)
+        console.log(`Ronda ${contador}`)
+        contador++
+        if(contador >= 6){
+            botones.forEach((btn)=>{
+                btn.disabled=true
+            })
+            final(puntaje_jugadores)
+        }
     })
 })
 
